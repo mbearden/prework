aaaa58986be51a8764e58346699efd734f0ae30b
mbearden <114111309+mbearden@users.noreply.github.com>
2022-09-29 19:29:45 -0500

    Update excersise1.js

--- excersise1.js
+++ excersise1.js
@@ -1,6 +1,5 @@
-("there once was a writer of code")
-("who wanted to learn JS and node')
-('they said "once I can learn,"')
-("'i'll increase what i earn.'")
-("So now they're in full programmer mode!")
-")
+console.log("there once was a writer of code");
+console.log("who wanted to learn JS and node");
+console.log('they said "once I can learn,"');
+console.log("'i'll increase what i earn.'");
+console.log("So now they're in full programmer mode");
