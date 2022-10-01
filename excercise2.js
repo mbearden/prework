50b1fb17b4de271d7551a28e6ce932d0df64d13e
mbearden <114111309+mbearden@users.noreply.github.com>
2022-10-01 13:44:55 -0500

    Update excercise2.js

--- excercise2.js
+++ excercise2.js
@@ -1,1 +1,6 @@
-//test
+console.log("this is a string, it has quotes.");
+console.log(100);
+console.log("100");
+console.log("my name is");
+console.log("my age is");
+console.log("my zip code is");
