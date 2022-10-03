b4ba6538a8415154b41c590abdb53494f93ab166
mbearden <114111309+mbearden@users.noreply.github.com>
2022-10-03 13:43:21 -0500

    Create excercise5.js

---
+++ excercise5.js
@@ -0,0 +1,6 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
