617a1724a3254ebd6d633a586ad3113566ae3fa8
mbearden <114111309+mbearden@users.noreply.github.com>
2022-10-03 13:34:59 -0500

    Create excersise4.js

---
+++ excersise4.js
@@ -0,0 +1,39 @@
+console.log("5 + 4 = ");
+console.log(5 + 4);
+
+console.log("10 - 7 = ");
+console.log(10 - 7);
+
+console.log("6 * 3 = ");
+console.log(6 * 3);
+
+console.log("18 / 2 = ");
+console.log(18 / 2);
+
+console.log("4 to the 2nd power is: ");
+console.log(4 ** 2);
+
+console.log("The remainder when 25 is divided by 4 is: ");
+console.log(25 % 4);
+
+console.log("13 + 8 / 2 - 1 = ");
+console.log(13 + 8 / 2 - 1); //What will this equal? Remember Order of Operations!
+
+console.log("(13 + 8) / (2 - 1) = ");
+console.log((13 + 8) / (2 - 1));
+ //What will this equal? Is it the same as the previous line?
+
+ console.log("The sum of 9 and 15 is: ");
+ console.log(9+15); 
+ console.log("The difference between 100 and 48 is: ");
+ console.log(100-48);
+ console.log("The product of 56 and 32 is: ");
+ console.log(56*32); //change this line
+ console.log("The quotient of 198 and 6 is: ");
+ console.log(198/6); //change this line
+ console.log("3 raised to the 5th power is: ");
+ console.log(3**5); //change this line
+ console.log("The remainder when 45 is divided by 7 is: ")
+ console.log(45/7); //change this line
+ console.log("The product of 5 and 7 added to the quotient of 12 and 3 is: ");
+ console.log(5*7)+(12/3); //change this line
