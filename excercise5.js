297ec28738360551a27fd1d2aa185741b9e40a9c
mbearden <114111309+mbearden@users.noreply.github.com>
2022-10-03 13:47:13 -0500

    Update excercise5.js
    ex 5

--- excercise5.js
+++ excercise5.js
@@ -4,3 +4,7 @@ console.log("True or false: 19 is not equal 11");
 console.log(19 != 11)
 console.log("True or false: 10 is greater than or equal to 10");
 console.log(10 >= 10);
+console.log(); // ==, true
+console.log(); // >, false
+console.log(); // <=, false
+console.log(); // !=, true
